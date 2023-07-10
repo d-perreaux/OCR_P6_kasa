0a147f10c4daad9e1db9e4800ec5b0aee2967e03
Dimitri PERREAUX <dimitri.perreaux@gmail.com>
2023-07-10 07:06:19 +0200

    component(Router): clearer router
    add a specific router folder

--- src/index.jsx
+++ src/index.jsx
@@ -2,30 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.scss';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import BaseLayout from './components/BaseLayout';
-import Home from './pages/Home';
-import Accomodation from './pages/Accomodation';
-import About from './pages/About';
-import Test from './pages/Test';
 
-import ErrorPage from './pages/ErrorPage';
+import RouterComponent from './router/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <BaseLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/accomodation/advertId" element={<Accomodation />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/error" element={<ErrorPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </BaseLayout>
-    </Router>
-  </React.StrictMode>
+  <RouterComponent />
 );
 
---
+++ src/router/Router.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import BaseLayout from '../components/BaseLayout';
+import Home from '../pages/Home';
+import Accomodation from '../pages/Accomodation';
+import About from '../pages/About';
+import ErrorPage from '../pages/ErrorPage';
+
+function RouterComponent() {
+    return (
+        <React.StrictMode>
+            <Router>
+                <BaseLayout>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/accomodation/advertId" element={<Accomodation />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/error" element={<ErrorPage />} />
+                        <Route path="*" element={<ErrorPage />} />
+                    </Routes>
+                </BaseLayout>
+            </Router>
+        </React.StrictMode>
+    )
+}
+
+export default RouterComponent
